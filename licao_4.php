286a180122e57e79fb0c818858d82078d7b891cf
Kvsvicentini <kaua.vicentini@gmail.com>
2025-04-01 22:12:00 -0300

    lição em sala

--- licao_4.php
+++ licao_4.php
@@ -1,13 +1,32 @@
 <?php 
- 	$num_1 = 30;
- 	$num_2 = 5;
- 	$num_3 = 20;
+ 	$num_1 = 8;
+ 	$num_2 = 2;
+ 	$num_3 = 6;
 
- 	if ($num_1 > $num_2) {
- 		if ($num_1 > $num_3) {
- 			if ($num_2 > $num_3) {
- 				echo ($num_1 "<br/>" $num_3 "<br/>" $num_2 );
+ 	if ($num_1 < $num_2) {
+ 		if ($num_1 < $num_3) {
+ 			if ($num_2 < $num_3) {
+ 				echo ($num_1 . " <br/> " . $num_2 . " <br/> " . $num_3);
  			}
+ 			else{
+ 				echo ($num_1 . " <br/> " . $num_3 . " <br/> " . $num_2);
+ 			}
+ 		}
+ 		else {
+ 			echo ($num_3 . " <br/> " . $num_1 . " <br/> " . $num_2);
+ 		}
+ 	} 
+ 	else{
+ 		if ($num_2 < $num_3) {
+ 			if ($num_3 < $num_1) {
+ 				echo ($num_2 . " <br/> " . $num_3 . " <br/> " . $num_1);
+ 			}
+ 			else {
+ 				echo ($num_2 . " <br/> " . $num_1 . " <br/> " . $num_3);
+ 			}
+ 		}
+ 		else {
+ 			echo ($num_3 . " <br/> " . $num_2 . " <br/> " . $num_1);
  		}
  	}
 
---
+++ licao_5.php
@@ -0,0 +1,31 @@
+<?php 
+	$height_1 = 0;
+	$height_2 = 0;
+	$gender_1 = "Feminino";
+	$gender_2 = "Masculino";
+	$cont = 0;
+	$media = 0;
+	$sum = 0;
+	$age_1 = 25;
+	$age_2 = 20;
+
+	if ($height_1 > $height_2) {
+		echo ("A maior altura é " . $height_1 . " e a menor altura é " . $height_2);
+	}
+	else {
+		echo ("A maior altura é " . $height_2 . " e a menor altura é " . $height_1);
+	}
+	if ($gender_1 == "Feminino") {
+		$sum = $sum + $height_1
+		$media = $sum / $cont
+		echo ("A média de altura das mulheres é " . $media);
+	}
+	if ($gender_2 == "Masculino" && $age_1 > $age_2) {
+		
+	}
+
+
+
+
+
+ ?>
