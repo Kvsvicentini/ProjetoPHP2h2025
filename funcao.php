a514a587f1ef6a81ecfa82c3c8d5c133b85f70e0
Kvsvicentini <kaua.vicentini@gmail.com>
2025-04-07 20:16:04 -0300

    Atividade em sala do dia 07/04/2025

---
+++ funcao.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$num_1 = 10;
+	$num_2 = 20;
+
+	//cirando uma função
+	function someValor($v1, $v2){
+		
+		return ($v1 + $v2);
+		
+	}
+
+	//executando uma função
+	echo "A soma de num_1 + num_2 é: " . someValor($num_1, $num_2);
+
+ ?>
