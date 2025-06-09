39b7f5a1d451aa39db1cc102e53f5c399ae465f1
Kvsvicentini <kaua.vicentini@gmail.com>
2025-06-09 20:26:26 -0300

    Aprendendo mais sobre o uso de formulários

--- formulario.php
+++ formulario.php
@@ -21,9 +21,44 @@
 		maxlength="50" size="50" 
 		required autocomplete="off">
 		<br>
-		<input type="submit" name="enviar" value="Entrar">
 		<input type="reset" name="limpar" value="limpar">
-		
+
+		<h2>Escolha uma linguagem de programação</h2>
+		  <input type="checkbox" id="java" name="java" value="Java">
+		  <label for="java"> JAVA</label><br>
+		  <input type="checkbox" id="python" name="python" value="Python">
+		  <label for="python"> PYTHON</label><br>
+		  <input type="checkbox" id="php" name="php" value="PHP">
+		  <label for="php"> PHP</label>
+
+		  <h3>Qual o seu estado civil?</h3>
+		  <input type="radio" id="estado_1" name="escolha" value="Casado">
+		  <label for="escolha"> Casado</label><br>
+		  <input type="radio" id="estado_2" name="escolha" value="Solteiro">
+		  <label for="escolha"> Solteiro</label><br>
+		  
+		  <h2>Inputs escondidas</h2>
+
+		  <input type="hidden" name="user" value="admin"><br>
+		  <input type="hidden" name="nivel" value="2"><br>
+
+		  <h2>Escolha um estado</h2>
+		  <select name="estado">
+		  	<option value="SP">São Paulo</option>
+		  	<option value="MG">Minas Gerais</option>
+		  	<option value="BA">Bahia</option>
+		  	<option value="CE">Ceará</option>
+		  	<option value="RJ">Rio de Janeiro</option>
+		  </select>
+
+		  <h2>Deixe a sua mensagem</h2>
+		  <br>
+		  <textarea name="msg" cols="50" rows="8">
+		  	
+		  </textarea>
+
+		  <input type="submit" name="enviar" value="Entrar">
+
 	</form>
 	<a href="">Enviar</a>
 </body>
--- resposta.php
+++ resposta.php
@@ -3,6 +3,38 @@
 	$user_nome = $_POST['nome'];
 	$user_email = $_POST['email'];
 
-	echo "Seu nome é $user_nome e seu e-mail é $user_email";
+	echo "Seu nome é $user_nome e seu e-mail é $user_email <br/>";
+	echo "<br/> Você escolheu a linguagem: <br/>";
+
+	if (isset($_POST['python'])) {
+		echo ("Python <br/>");
+	}
+	if (isset($_POST['java'])) {
+		echo ("Java <br/>");
+	}
+	if (isset($_POST['php'])) {
+		echo ("PHP <br/>");
+	}
+
+	echo "<br/> Seu estado civil é: ";
+
+	if (isset($_POST["escolha"])) {
+		echo $_POST['escolha'];
+	}
+
+	echo "<br/>";
+	echo "<br/> Recuperando valores das inputs escondidas: <br/>";
+
+	$user = $_POST['user'];
+	$nivel = $_POST['nivel'];
+
+	echo "<br/> Seu usuário é $user e seu nível é $nivel <br/>";
+
+	echo "<br/>";
+	echo "Estado selecionado: <br/>";
+	echo "O estado selecionado foi " . $_POST['estado'] . "<br/>";
+
+	echo "<br/>Recuperando a mensagem: <br/>";
+	echo "Sua mensagem foi: " . $_POST['msg'] . "<br/>";
 
  ?>
